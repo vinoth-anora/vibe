f8b882a8ff63120192f5b25912c7b39db5e7c3ba
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-25 17:29:07 +0530

    qwerfgthjk

--- a/ass4.py
+++ a/ass4.py
@@ -1,7 +1,7 @@
 car_makes = ['Honda', 'Toyota', 'Ford', 'Nissan', 'Hyundai']
 car_models = ['Honda Civic', 'Honda Accord', 'Toyota Corolla', 'Toyota Camry', 'Ford Fusion', 'Ford Escape',
                   'Nissan Sentra', 'Hyundai Elantra']
-
+"qwertyuiokjhgfds"
 "guru"
 def dictionary():
     global car_makes
